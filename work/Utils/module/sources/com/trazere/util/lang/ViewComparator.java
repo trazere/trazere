b60d1727ce031d825373bdbc365ba94c366f8b27
trazere@gmail.com <trazere@gmail.com@7daa012c-bbdc-11de-ad03-79fce359684b>
2009-10-24 11:45:01 +0000

    Utils  * Improved `ViewComparator`.  * Improved `RecordComparator`.

--- work/Utils/module/sources/com/trazere/util/lang/ViewComparator.java
+++ work/Utils/module/sources/com/trazere/util/lang/ViewComparator.java
@@ -15,10 +15,9 @@
  */
 package com.trazere.util.lang;
 
+import com.trazere.util.collection.LazyMap;
 import com.trazere.util.function.Function1;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * The {@link ViewComparator} abstract class is a comparator working according some value computed from the compared objects. It applies some kind of view over
@@ -39,13 +38,12 @@ implements Comparator<T> {
 	 * @param <V> Type of the values to compore.
 	 * @param function The function computing the view.
 	 * @param comparator The comparator to use.
-	 * @param cache Flag indicating whether the cache if enabled or not.
 	 * @return The built iterator.
 	 */
-	public static <T, V> ViewComparator<T, V> build(final Function1<? super T, ? extends V, ? extends RuntimeException> function, final Comparator<? super V> comparator, final boolean cache) {
+	public static <T, V> ViewComparator<T, V> build(final Function1<? super T, ? extends V, ? extends RuntimeException> function, final Comparator<? super V> comparator) {
 		assert null != function;
 		
-		return new ViewComparator<T, V>(comparator, cache) {
+		return new ViewComparator<T, V>(comparator) {
 			@Override
 			protected V computeValue(final T object) {
 				return function.evaluate(object);
@@ -53,55 +51,41 @@ implements Comparator<T> {
 		};
 	}
 	
+	/**
+	 * Build a comparation using the function.
+	 * 
+	 * @param <T> Type of the compared objets.
+	 * @param <V> Type of the values to compore.
+	 * @param function The function computing the view.
+	 * @param comparator The comparator to use.
+	 * @param cache Flag indicating whether the cache if enabled or not.
+	 * @return The built iterator.
+	 */
+	public static <T, V> ViewComparator<T, V> build(final Function1<? super T, ? extends V, ? extends RuntimeException> function, final Comparator<? super V> comparator, final boolean cache) {
+		if (cache) {
+			return build(LazyMap.build(function), comparator);
+		} else {
+			return build(function, comparator);
+		}
+	}
+	
 	/** Comparator to use. */
 	protected final Comparator<? super V> _comparator;
-	
-	/** Flag indicating whether the cache if enabled or not. */
-	protected final boolean _cache;
-	
-	/** Cached values identified by the objects they where computed from. */
-	protected final Map<T, V> _values = new HashMap<T, V>();
 	
 	/**
 	 * Instantiate a new comparator.
 	 * 
 	 * @param comparator The comparator to use. May be <code>null</code>.
-	 * @param cache Flag indicating whether the cache if enabled or not.
 	 */
-	public ViewComparator(final Comparator<? super V> comparator, final boolean cache) {
+	public ViewComparator(final Comparator<? super V> comparator) {
 		assert null != comparator;
 		
 		// Initialization.
 		_comparator = comparator;
-		_cache = cache;
 	}
 	
 	public int compare(final T object1, final T object2) {
-		final V value1 = _cache ? getValue(object1) : computeValue(object1);
-		final V value2 = _cache ? getValue(object2) : computeValue(object2);
-		return LangUtils.compare(value1, value2, _comparator);
-	}
-	
-	/**
-	 * Get the value to use for comparison for the given object. This methods uses and populates the cache.
-	 * 
-	 * @param object Compared object whose comparison is retrieved. May be <code>null</code>.
-	 * @return The comparison value. May be <code>null</code>.
-	 */
-	protected V getValue(final T object) {
-		// Read the cache.
-		final V cachedValue = _values.get(object);
-		if (null != cachedValue) {
-			return cachedValue;
-		}
-		
-		// Compute the value.
-		final V value = computeValue(object);
-		
-		// Fill the cache.
-		_values.put(object, value);
-		
-		return value;
+		return LangUtils.compare(computeValue(object1), computeValue(object2), _comparator);
 	}
 	
 	/**
@@ -111,8 +95,4 @@ implements Comparator<T> {
 	 * @return The comparison value. May be <code>null</code>.
 	 */
 	protected abstract V computeValue(final T object);
-	
-	public void flushCache() {
-		_values.clear();
-	}
 }
