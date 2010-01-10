bb8669a934a658d35cda0349fe9d88f72cc2dac3
trazere <trazere@gmail.com>
2010-01-10 15:53:57 +0000

    * Updated the copyright notices.

--- work/Utils/module/sources/com/trazere/util/record/RecordClosure.java
+++ work/Utils/module/sources/com/trazere/util/record/RecordClosure.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2006-2009 Julien Dufour
+ *  Copyright 2006-2010 Julien Dufour
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
