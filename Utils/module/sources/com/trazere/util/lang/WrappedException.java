19c44a0eb2258888351c5e096bee874473d87161
trazere <trazere@gmail.com>
2010-04-15 12:04:59 +0000

    Utils  * Added various exception factories.  * Added the exception wrapping exception.

---
+++ Utils/module/sources/com/trazere/util/lang/WrappedException.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright 2006-2010 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.util.lang;
+
+/**
+ * The {@link WrappedException} exceptions aim to wrap other exceptions to propagate them out of methods which throw nothing.
+ */
+public class WrappedException
+extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Instantiate a new exception using the given cause.
+	 * 
+	 * @param cause Cause of the exception.
+	 */
+	public WrappedException(final Throwable cause) {
+		super(cause);
+	}
+}
