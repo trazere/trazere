f31087f583ffe1c43c72c012e5b190591927d1c9
trazere@gmail.com <trazere@gmail.com@7daa012c-bbdc-11de-ad03-79fce359684b>
2011-02-26 20:18:37 +0000

    * Improved the building shell scripts.  * Added the building shell scripts for ''Windows''.

--- work/project/build.sh
+++ work/project/build.sh
@@ -1,33 +1,25 @@
-#!/bin/sh -e
-
-# Check JAVA_HOME
-if [ -z "$JAVA_HOME" ]
-then
-    echo "Please set the JAVA_HOME environment variable."
-    exit
-fi
-
-# Check ANT_HOME
-if [ -z "$ANT_HOME" ]
-then
-    echo "Please set the ANT_HOME environment variable."
-    exit
-fi
+#!/bin/bash
 
 # OS specific support.
 cygwin=false
-case "`uname`" in
+case "$(uname)" in
     CYGWIN*) cygwin=true ;;
 esac
 
-# Run the Ant script.
-SPINOZA_HOME="`pwd`"
-
-ENDORSED_SPINOZA_HOME="$SPINOZA_HOME"
+# Compute the project home.
+PROJECT_HOME=$(cd $(dirname "$0") ; pwd)
 if $cygwin
 then
-    ENDORSED_SPINOZA_HOME=`cygpath -m "$ENDORSED_SPINOZA_HOME"`
+    PROJECT_HOME=$(cygpath -m "$PROJECT_HOME")
 fi
-export ANT_OPTS="-Xmx1024m -Djava.endorsed.dirs=$ENDORSED_SPINOZA_HOME/endorsed"
 
-"$ANT_HOME/bin/ant" $*
+# Check ANT_HOME.
+if [ -z "$ANT_HOME" ]
+then
+    echo "Please set the ANT_HOME environment variable."
+    exit 1
+fi
+
+# Run the Ant script.
+export ANT_OPTS="-Xmx1024m -Djava.endorsed.dirs=$PROJECT_HOME/endorsed"
+exec "$ANT_HOME/bin/ant" "$@"
