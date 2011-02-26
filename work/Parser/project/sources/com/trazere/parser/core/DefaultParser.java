124bb8e551623dfbceed06a882191f2b58b1d9ee
trazere@gmail.com <trazere@gmail.com@7daa012c-bbdc-11de-ad03-79fce359684b>
2011-02-26 23:56:20 +0000

    Parser  * Externalized some parsers in their own class.

---
+++ work/Parser/project/sources/com/trazere/parser/core/DefaultParser.java
@@ -0,0 +1,72 @@
+/*
+ *  Copyright 2006-2011 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.parser.core;
+
+import com.trazere.parser.Parser;
+import com.trazere.parser.ParserException;
+import com.trazere.util.lang.HashCode;
+import com.trazere.util.lang.LangUtils;
+import com.trazere.util.type.Maybe;
+import com.trazere.util.type.TypeUtils;
+
+/**
+ * DOCME
+ * 
+ * @param <Token>
+ * @param <Result>
+ */
+public class DefaultParser<Token, Result>
+extends Sequence1Parser<Token, Maybe<Result>, Result> {
+	public DefaultParser(final Parser<Token, Maybe<Result>> subParser, final Result defaultResult, final String description) {
+		super(subParser, description);
+		
+		// Initialization.
+		_defaultResult = defaultResult;
+	}
+	
+	// Parser.
+	
+	protected final Result _defaultResult;
+	
+	@Override
+	protected Result combine(final Maybe<Result> subResult1)
+	throws ParserException {
+		return TypeUtils.get(subResult1, _defaultResult);
+	}
+	
+	// Object.
+	
+	@Override
+	public int hashCode() {
+		final HashCode result = new HashCode(this);
+		result.append(_description);
+		result.append(_subParser1);
+		result.append(_defaultResult);
+		return result.get();
+	}
+	
+	@Override
+	public boolean equals(final Object object) {
+		if (this == object) {
+			return true;
+		} else if (null != object && getClass().equals(object.getClass())) {
+			final DefaultParser<?, ?> parser = (DefaultParser<?, ?>) object;
+			return LangUtils.equals(_description, parser._description) && _subParser1.equals(parser._subParser1) && LangUtils.equals(_defaultResult, parser._defaultResult);
+		} else {
+			return false;
+		}
+	}
+}
---
+++ work/Parser/project/sources/com/trazere/parser/core/IdentityParser.java
@@ -0,0 +1,62 @@
+/*
+ *  Copyright 2006-2011 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.parser.core;
+
+import com.trazere.parser.Parser;
+import com.trazere.util.lang.HashCode;
+import com.trazere.util.lang.LangUtils;
+
+/**
+ * DOCME
+ * 
+ * @param <Token>
+ * @param <Result>
+ */
+public class IdentityParser<Token, Result>
+extends Sequence1Parser<Token, Result, Result> {
+	public IdentityParser(final Parser<Token, ? extends Result> subParser, final String description) {
+		super(subParser, description);
+	}
+	
+	// Parser.
+	
+	@Override
+	protected Result combine(final Result result) {
+		return result;
+	}
+	
+	// Object.
+	
+	@Override
+	public int hashCode() {
+		final HashCode result = new HashCode(this);
+		result.append(_description);
+		result.append(_subParser1);
+		return result.get();
+	}
+	
+	@Override
+	public boolean equals(final Object object) {
+		if (this == object) {
+			return true;
+		} else if (null != object && getClass().equals(object.getClass())) {
+			final IdentityParser<?, ?> parser = (IdentityParser<?, ?>) object;
+			return LangUtils.equals(_description, parser._description) && _subParser1.equals(parser._subParser1);
+		} else {
+			return false;
+		}
+	}
+}
