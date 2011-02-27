16270d63532ea219c3e094584a965e4907338aa5
trazere <trazere@gmail.com>
2011-02-27 15:45:46 +0000

    Utils  * Added a type parameter for the representations of the value serializers.

--- work/Utils/project/sources/com/trazere/util/value/ValueSerializer.java
+++ work/Utils/project/sources/com/trazere/util/value/ValueSerializer.java
@@ -16,36 +16,44 @@
 package com.trazere.util.value;
 
 /**
- * The {@link ValueSerializer} interface defines functions for serializing/deserializing values to/from string representations.
+ * The {@link ValueSerializer} interface defines serialization/deserialization functions.
  * 
  * @param <T> Type of the values.
+ * @param <R> Type of the representations.
  * @param <X> Type of the exceptions.
  */
-public interface ValueSerializer<T, X extends Exception> {
+public interface ValueSerializer<T, R, X extends Exception> {
 	/**
-	 * Get the type of the values serialized by the receiver reader.
+	 * Gets the type of the values supported by the receiver serializer.
 	 * 
 	 * @return The Java type of the values.
 	 */
 	public Class<T> getValueClass();
 	
 	/**
-	 * Serialize the given value to its corresponding string representation.
+	 * Gets the type of the representation supported by the receiver serializer.
+	 * 
+	 * @return The Java type of the serialized values.
+	 */
+	public Class<R> getRepresentationClass();
+	
+	/**
+	 * Serializes the given value to its corresponding representation.
 	 * 
 	 * @param value The value.
 	 * @return The representation of the value.
 	 * @throws X When the serialization fails.
 	 */
-	public String serialize(final T value)
+	public R serialize(final T value)
 	throws X;
 	
 	/**
-	 * Deserialize the given string representation to its corresponding value.
+	 * Deserializes the given representation to its corresponding value.
 	 * 
 	 * @param representation The representation of the value.
 	 * @return The value.
 	 * @throws X When the deserialization fails.
 	 */
-	public T deserialize(final String representation)
+	public T deserialize(final R representation)
 	throws X;
 }
