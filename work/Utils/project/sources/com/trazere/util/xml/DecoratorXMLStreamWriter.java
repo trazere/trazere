6cc98090bb7f0d8336ec076bf28375c0d5041332
trazere <trazere@gmail.com>
2012-01-12 09:44:00 +0000

    Misc  * Updated the copyrights.

--- work/Utils/project/sources/com/trazere/util/xml/DecoratorXMLStreamWriter.java
+++ work/Utils/project/sources/com/trazere/util/xml/DecoratorXMLStreamWriter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2006-2011 Julien Dufour
+ *  Copyright 2006-2012 Julien Dufour
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
