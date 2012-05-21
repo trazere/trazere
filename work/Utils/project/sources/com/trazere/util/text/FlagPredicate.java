db618218afbfc02d86a3b9713f7c2b6b98323217
trazere <trazere@gmail.com>
2012-05-21 11:28:38 +0000

    Utils  * Removed the `TextUtils.strip(String)` and `TextUtils.unstrip(String)` methods.

--- work/Utils/project/sources/com/trazere/util/text/FlagPredicate.java
+++ work/Utils/project/sources/com/trazere/util/text/FlagPredicate.java
@@ -25,8 +25,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-// TODO: implements Predicate
-
 /**
  * The {@link FlagPredicate} class represents predicates over sets of flags.
  * <p>
