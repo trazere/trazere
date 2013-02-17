7dd1e2402dd82398693b9b94bd559a51a6e2aac8
trazere <trazere@gmail.com>
2013-02-17 13:29:41 +0000

    Ant  * Moved the Ant utils in their own project.

--- Ant/project/sources/com/trazere/ant/TargetTask.java
+++ Ant/project/sources/com/trazere/ant/TargetTask.java
@@ -13,9 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.trazere.util;
-
-// TODO: delete
+package com.trazere.ant;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Target;
