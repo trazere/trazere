480cd3792f3af37c8b7345bc7302a67f062fc3df
trazere <trazere@gmail.com>
2013-10-29 15:11:58 +0000

    Utils  * Generalized the extractor combinaison method.  * Generalized the type of failures of retry functions.  * Generalized the XML adapter factory based on a value serializer.

--- work/Utils/project/src/main/java/com/trazere/util/function/Extractors.java
+++ work/Utils/project/src/main/java/com/trazere/util/function/Extractors.java
@@ -57,7 +57,7 @@ public class Extractors {
 	 * @param f The inned extractor.
 	 * @return The built extractor.
 	 */
-	public static <T1, T2, T3, X extends Exception> Function1<T1, Maybe<T3>, X> compose(final Function1<? super T2, ? extends Maybe<? extends T3>, ? extends X> g, final Function1<? super T1, ? extends Maybe<? extends T2>, X> f) {
+	public static <T1, T2, T3, X extends Exception> Function1<T1, Maybe<T3>, X> compose(final Function1<? super T2, ? extends Maybe<? extends T3>, ? extends X> g, final Function1<? super T1, ? extends Maybe<? extends T2>, ? extends X> f) {
 		return Functions.compose(Maybe.mapFilterFunction(g), f);
 	}
 	
