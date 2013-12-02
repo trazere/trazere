668d9fdbc07aba619bdd4c715582b51786ded4f2
trazere <trazere@gmail.com>
2013-12-02 15:04:51 +0000

    Utils  * Moved JAXB type parse and print methods into the respective JAXB adapter classes.  * Handle null values correctly in all JAXB adapters.

--- Utils/project/main/sources/com/trazere/util/xml/TimeAdapter.java
+++ Utils/project/main/sources/com/trazere/util/xml/TimeAdapter.java
@@ -15,21 +15,52 @@
  */
 package com.trazere.util.xml;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.xml.bind.DatatypeConverter;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
- * The {@link TimeAdapter} class provides JAXB adpater for times.
+ * The {@link TimeAdapter} class provides JAXB adpaters for times.
  */
 public class TimeAdapter
 extends XmlAdapter<String, Date> {
 	@Override
 	public Date unmarshal(final String value) {
-		return DatatypeConverters.parseTime(value);
+		return null != value ? parse(value) : null;
 	}
 	
 	@Override
 	public String marshal(final Date value) {
-		return DatatypeConverters.printTime(value);
+		return null != value ? print(value) : null;
+	}
+	
+	/**
+	 * Parses the given time.
+	 * 
+	 * @param representation Representation to parse.
+	 * @return The time.
+	 * @throws IllegalArgumentException When the representation is invalid.
+	 */
+	public static Date parse(final String representation)
+	throws IllegalArgumentException {
+		assert null != representation;
+		
+		return DatatypeConverter.parseTime(representation).getTime();
+	}
+	
+	/**
+	 * Formats the given time.
+	 * 
+	 * @param date Time to format.
+	 * @return The representation.
+	 */
+	public static String print(final Date date) {
+		assert null != date;
+		
+		final Calendar cal = new GregorianCalendar();
+		cal.setTime(date);
+		return DatatypeConverter.printTime(cal);
 	}
 }
