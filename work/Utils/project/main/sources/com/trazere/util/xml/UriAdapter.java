0be943336ebe4463a49e7ec34b10d1b864305dbd
trazere@gmail.com <trazere@gmail.com@7daa012c-bbdc-11de-ad03-79fce359684b>
2013-12-02 14:51:27 +0000

    Utils  * Added function factories for building URLs and URIs.  * Added JAXB adapters for URLs and URIs.

---
+++ work/Utils/project/main/sources/com/trazere/util/xml/UriAdapter.java
@@ -0,0 +1,70 @@
+/*
+ *  Copyright 2006-2013 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.util.xml;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+// TODO: move serialization methods in adapters in trazere
+// TODO: handle null correctly in adpaters in trazere
+
+/**
+ * The {@link UriAdapter} class provides JAXB adpaters for URIs.
+ */
+public class UriAdapter
+extends XmlAdapter<String, URI> {
+	@Override
+	public URI unmarshal(final String value)
+	throws IllegalArgumentException {
+		return null != value ? parse(value) : null;
+	}
+	
+	@Override
+	public String marshal(final URI value) {
+		return null != value ? print(value) : null;
+	}
+	
+	/**
+	 * Parses the given uri.
+	 * 
+	 * @param representation Representation to parse.
+	 * @return The uri.
+	 * @throws IllegalArgumentException When the representation is invalid.
+	 */
+	public static URI parse(final String representation)
+	throws IllegalArgumentException {
+		assert null != representation;
+		
+		try {
+			return new URI(representation);
+		} catch (final URISyntaxException exception) {
+			throw new IllegalArgumentException("Invalid uri representation \"" + representation + "\"", exception);
+		}
+	}
+	
+	/**
+	 * Formats the given uri.
+	 * 
+	 * @param uri Uri to print.
+	 * @return The representation.
+	 */
+	public static String print(final URI uri) {
+		assert null != uri;
+		
+		return uri.toString();
+	}
+}
