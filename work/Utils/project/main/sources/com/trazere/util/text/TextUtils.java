a3f87afef411f74d6fdb196621729de1a8f720a9
trazere <trazere@gmail.com>
2014-01-24 15:43:20 +0000

    Utils  * Generalized `TextUtils.parseNumber(...)`.

--- work/Utils/project/main/sources/com/trazere/util/text/TextUtils.java
+++ work/Utils/project/main/sources/com/trazere/util/text/TextUtils.java
@@ -498,12 +498,15 @@ public class TextUtils {
 	 * This method synchronizes the format.
 	 * 
 	 * @param <T> Type of the number to parse.
+	 * @param <CX> Type of the conversion exceptions.
+	 * 
 	 * @param format The number format.
 	 * @param converter The extractor of the {@link Number} instance of the excepted type.
 	 * @param representation The representation to parse.
 	 * @return The parsed number.
+	 * @throws CX When the conversion to the result type fails. 
 	 */
-	public static <T extends Number> Maybe<T> parseNumber(final NumberFormat format, final Function1<Number, T, RuntimeException> converter, final String representation) {
+	public static <T extends Number, CX extends Exception> Maybe<T> parseNumber(final NumberFormat format, final Function1<? super Number, ? extends T, CX> converter, final String representation) throws CX {
 		assert null != format;
 		assert null != converter;
 		assert null != representation;
@@ -512,7 +515,7 @@ public class TextUtils {
 			final ParsePosition position = new ParsePosition(0);
 			final Number number = format.parse(representation, position);
 			if (null != number && position.getIndex() == representation.length()) {
-				return Maybe.some(converter.evaluate(number));
+				return Maybe.<T>some(converter.evaluate(number));
 			} else {
 				return Maybe.none();
 			}
