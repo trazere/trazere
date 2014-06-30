6da89f1618a5b09b2a495040c87e43cffe277973
trazere <trazere@gmail.com>
2014-06-30 21:21:20 +0000

    Core  * Parted procedures.  * Renamed `Procedure0` to `Effect`.  * Renamed `Procedure1` to `Procedure`.  * Added `Procedure3`, `Procedure4` and `Procedure5`.  * Ported accumulators.  * Renamed `Accumulator1` to `Accumulator`.

---
+++ Core/project/main/sources/com/trazere/core/imperative/Accumulator.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2006-2013 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.core.imperative;
+
+/**
+ * The {@link Accumulator} interface defines left folding functions over an internal state.
+ * <p>
+ * Accumulators are dual to iterators.
+ * 
+ * @param <A> Type of the arguments.
+ * @param <S> Type of the state.
+ */
+public interface Accumulator<A, S>
+extends Procedure<A> {
+	/**
+	 * Accumulates the given value into the receiver accumulator.
+	 * 
+	 * @param value Value to accumulate.
+	 */
+	void add(A value);
+	
+	/**
+	 * Accumulates the given values into the receiver accumulator.
+	 * <p>
+	 * Values are accumulated in the order they are iterated.
+	 * 
+	 * @param values Values to accumulate.
+	 */
+	void addAll(Iterable<? extends A> values);
+	
+	/**
+	 * Gets the current state of the receiver accumulator.
+	 * 
+	 * @return The state.
+	 */
+	S get();
+}
