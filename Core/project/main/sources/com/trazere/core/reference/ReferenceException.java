e85683e839fdf0968556f325da1df5262e9ed3b2
trazere <trazere@gmail.com>
2014-07-05 11:53:42 +0000

    Core  * Ported references.

---
+++ Core/project/main/sources/com/trazere/core/reference/ReferenceException.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright 2006-2013 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.core.reference;
+
+import com.trazere.core.lang.ThrowableFactory;
+
+/**
+ * {@link ReferenceException} exceptions are thrown when reference related errors occur.
+ */
+public class ReferenceException
+extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Instantiates a new exception.
+	 */
+	public ReferenceException() {
+		super();
+	}
+	
+	/**
+	 * Instantiates a new exception using the given message.
+	 * 
+	 * @param message Details about the exception.
+	 */
+	public ReferenceException(final String message) {
+		super(message);
+	}
+	
+	/**
+	 * Instantiates a new exception using the given cause.
+	 * 
+	 * @param cause Cause of the exception.
+	 */
+	public ReferenceException(final Throwable cause) {
+		super(cause);
+	}
+	
+	/**
+	 * Instantiates a new exception using the given message and cause.
+	 * 
+	 * @param message Details about the exception.
+	 * @param cause Cause of the exception.
+	 */
+	public ReferenceException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+	
+	/** Factory of {@link ReferenceException}. */
+	public static final ThrowableFactory<ReferenceException> FACTORY = new ThrowableFactory<ReferenceException>() {
+		@Override
+		public ReferenceException build() {
+			return new ReferenceException();
+		}
+		
+		@Override
+		public ReferenceException build(final String message) {
+			return new ReferenceException(message);
+		}
+		
+		@Override
+		public ReferenceException build(final Throwable cause) {
+			return new ReferenceException(cause);
+		}
+		
+		@Override
+		public ReferenceException build(final String message, final Throwable cause) {
+			return new ReferenceException(message, cause);
+		}
+	};
+}
---
+++ Core/project/main/sources/com/trazere/core/reference/References.java
@@ -0,0 +1,95 @@
+/*
+ *  Copyright 2006-2013 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.core.reference;
+
+import com.trazere.core.util.Maybe;
+
+/**
+ * The {@link References} class provides various factories of references.
+ * 
+ * @see Reference
+ */
+public class References {
+	/**
+	 * Builds an unset reference.
+	 * 
+	 * @param <T> Type of the referenced value.
+	 * @return The built reference.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> Reference<T> empty() {
+		return (Reference<T>) EMTPY;
+	}
+	
+	private static final Reference<?> EMTPY = new BaseReference<Object>() {
+		@Override
+		public boolean isSet() {
+			return false;
+		}
+		
+		@Override
+		public Object get()
+		throws ReferenceNotSetException {
+			throw new ReferenceNotSetException();
+		}
+		
+		@Override
+		public Maybe<Object> asMaybe() {
+			return Maybe.none();
+		}
+	};
+	
+	/**
+	 * Builds a reference set with the given value.
+	 * 
+	 * @param <T> Type of the referenced value.
+	 * @param value Value to set.
+	 * @return The built reference.
+	 */
+	public static <T> Reference<T> fromValue(final T value) {
+		return new BaseReference<T>() {
+			@Override
+			public boolean isSet() {
+				return true;
+			}
+			
+			@Override
+			public T get() {
+				return value;
+			}
+			
+			@Override
+			public Maybe<T> asMaybe() {
+				return Maybe.some(value);
+			}
+		};
+	}
+	
+	/**
+	 * Builds a reference set with the given value if any.
+	 * 
+	 * @param <T> Type of the referenced value.
+	 * @param value Value to set.
+	 * @return The built reference.
+	 */
+	public static <T> Reference<T> fromValue(final Maybe<T> value) {
+		return value.isSome() ? fromValue(value.asSome().getValue()) : References.<T>empty();
+	}
+	
+	private References() {
+		// Prevent instantiation.
+	}
+}
