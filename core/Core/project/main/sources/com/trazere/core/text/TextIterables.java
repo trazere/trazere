570adfda994dac703d20a0b0f91b287d52676ef5
trazere <trazere@gmail.com>
2015-05-21 11:32:51 +0000

    Core  * Added a reader of a char sequence.  * Added factories of iterators from streams and readers.  * Added a factory of iterables from char sequences.

---
+++ core/Core/project/main/sources/com/trazere/core/text/TextIterables.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2006-2013 Julien Dufour
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.trazere.core.text;
+
+import com.trazere.core.imperative.IteratorUtils;
+import com.trazere.core.math.IntSequence;
+import java.util.Iterator;
+
+/**
+ * The {@link TextIterables} class provides various factories of {@link Iterable iterable} regarding text.
+ * 
+ * @see Iterable
+ */
+public class TextIterables {
+	/**
+	 * Builds an iterable from the given sequence of characters.
+	 * 
+	 * @param s Sequence of characters to iterate over.
+	 * @return The built iterable.
+	 */
+	public static Iterable<Character> fromCharSequence(final CharSequence s) {
+		assert null != s;
+		
+		return new Iterable<Character>() {
+			@Override
+			public Iterator<Character> iterator() {
+				return IteratorUtils.map(new IntSequence(0, s.length()).iterator(), i -> s.charAt(i));
+			}
+		};
+	}
+	
+	private TextIterables() {
+		// Prevent instantiation.
+	}
+}
