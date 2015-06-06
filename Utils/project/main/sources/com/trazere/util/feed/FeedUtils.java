ca16783fc763b0d976ccd05e29d2c59214ca6e7c
trazere <trazere@gmail.com>
2015-06-06 21:43:50 +0000

    Utils  * Added adaptation utilities between core and util classes.

--- Utils/project/main/sources/com/trazere/util/feed/FeedUtils.java
+++ Utils/project/main/sources/com/trazere/util/feed/FeedUtils.java
@@ -17,8 +17,11 @@ package com.trazere.util.feed;
 
 import com.trazere.util.collection.CheckedIterators;
 import com.trazere.util.collection.CollectionUtils;
+import com.trazere.util.lang.WrapException;
 import com.trazere.util.type.Maybe;
+import com.trazere.util.type.Tuple2;
 import java.util.Collection;
+import java.util.NoSuchElementException;
 
 /**
  * The {@link FeedUtils} class provides various utilities regarding feeds.
@@ -94,6 +97,112 @@ public class FeedUtils {
 	// TODO: add takeWhile
 	// TODO: add drop
 	// TODO: add dropWhile
+	
+	/**
+	 * Adapts the given util feed to a core feed.
+	 * 
+	 * @param <T> Type of the elements.
+	 * @param feed Util feed to adapt.
+	 * @return The adapted core feed.
+	 * @deprecated Use {@link com.trazere.core.collection.Feed}.
+	 */
+	@Deprecated
+	public static <T> com.trazere.core.collection.Feed<T> toFeed(final Feed<? extends T, ?> feed) {
+		assert null != feed;
+		
+		return new com.trazere.core.collection.Feed<T>() {
+			@Override
+			public boolean isEmpty() {
+				try {
+					return feed.isEmpty();
+				} catch (final Exception exception) {
+					throw new WrapException(exception);
+				}
+			}
+			
+			@Override
+			public T getHead()
+			throws NoSuchElementException {
+				try {
+					return feed.getHead();
+				} catch (final NoSuchElementException exception) {
+					throw exception;
+				} catch (final Exception exception) {
+					throw new WrapException(exception);
+				}
+			}
+			
+			@Override
+			public com.trazere.core.collection.Feed<? extends T> getTail()
+			throws NoSuchElementException {
+				try {
+					return toFeed(feed.getTail());
+				} catch (final NoSuchElementException exception) {
+					throw exception;
+				} catch (final Exception exception) {
+					throw new WrapException(exception);
+				}
+			}
+			
+			@Override
+			public com.trazere.core.util.Maybe<? extends com.trazere.core.util.Tuple2<? extends T, ? extends com.trazere.core.collection.Feed<? extends T>>> evaluate() {
+				try {
+					final Maybe<? extends Tuple2<? extends T, ? extends Feed<? extends T, ?>>> maybeItem = feed.evaluate();
+					if (maybeItem.isSome()) {
+						final Tuple2<? extends T, ? extends Feed<? extends T, ?>> item = maybeItem.asSome().getValue();
+						return com.trazere.core.util.Maybe.some(new com.trazere.core.util.Tuple2<T, com.trazere.core.collection.Feed<T>>(item.getFirst(), toFeed(item.getSecond())));
+					} else {
+						return com.trazere.core.util.Maybe.none();
+					}
+				} catch (final Exception exception) {
+					throw new WrapException(exception);
+				}
+			}
+		};
+	}
+	
+	/**
+	 * Adapts the given core feed to an util feed.
+	 * 
+	 * @param <E> Type of the elements.
+	 * @param feed Core feed to adapt.
+	 * @return The adapted util feed.
+	 * @deprecated Use {@link com.trazere.core.collection.Feed}.
+	 */
+	@Deprecated
+	public static <E> Feed<E, RuntimeException> fromFeed(final com.trazere.core.collection.Feed<? extends E> feed) {
+		assert null != feed;
+		
+		return new Feed<E, RuntimeException>() {
+			@Override
+			public boolean isEmpty() {
+				return feed.isEmpty();
+			}
+			
+			@Override
+			public E getHead()
+			throws NoSuchElementException {
+				return feed.getHead();
+			}
+			
+			@Override
+			public Feed<? extends E, ? extends RuntimeException> getTail()
+			throws NoSuchElementException {
+				return fromFeed(feed.getTail());
+			}
+			
+			@Override
+			public Maybe<? extends Tuple2<? extends E, ? extends Feed<? extends E, ? extends RuntimeException>>> evaluate() {
+				final com.trazere.core.util.Maybe<? extends com.trazere.core.util.Tuple2<? extends E, ? extends com.trazere.core.collection.Feed<? extends E>>> maybeItem = feed.evaluate();
+				if (maybeItem.isSome()) {
+					final com.trazere.core.util.Tuple2<? extends E, ? extends com.trazere.core.collection.Feed<? extends E>> item = maybeItem.asSome().getValue();
+					return Maybe.some(new Tuple2<E, Feed<E, RuntimeException>>(item.get1(), fromFeed(item.get2())));
+				} else {
+					return Maybe.none();
+				}
+			}
+		};
+	}
 	
 	private FeedUtils() {
 		// Prevent instantiation.
